c9fe5cedf3cb4f6581c18a2423825441160afedc
yls <405548753@qq.com>
2019-01-30 17:28:08 +0800

    更新上次图片

--- controllers/UploadController.php
+++ controllers/UploadController.php
@@ -1,10 +1,10 @@
 <?php
 namespace app\controllers;
 
-use Upload\Storage\FileSystem;
 use Upload\File;
 use app\library\Dir;
 use app\basic\BasicController;
+use woodlsy\upload\Upload;
 
 class UploadController extends BasicController
 {
@@ -14,32 +14,12 @@ class UploadController extends BasicController
         try {
             $path = \Yii::$app->basePath.'/web/upload/'.date('Ymd');
             Dir::directory($path);
-            $storage = new FileSystem($path);
-            $file = new File('file', $storage);
-            $new_filename = uniqid();
-            $file->setName($new_filename);
             
-            $file->addValidations(array(
-                // Ensure file is of type "image/png"
-                new \Upload\Validation\Mimetype(array('image/png', 'image/gif', 'image/jpeg')),
-            
-                // Ensure file is no larger than 5M (use "B", "K", M", or "G")
-                new \Upload\Validation\Size('1M')
-            ));
-            
-//             $data = array(
-//                 'name'       => $file->getNameWithExtension(),
-//                 'extension'  => $file->getExtension(),
-//                 'mime'       => $file->getMimetype(),    
-//                 'size'       => $file->getSize(),
-//                 'md5'        => $file->getMd5(),
-//                 'dimensions' => $file->getDimensions()
-//             );
-            $file->upload();
-            return $this->ajaxReturn(0, '上传成功', '/upload/'.date('Ymd').'/'.$file->getNameWithExtension());
+            $data = (new Upload())->setFieldName('file')->setMaxSize('1M')->setUploadPath($path)->upload();
+            return $this->ajaxReturn(0, '上传成功', '/upload/'.date('Ymd').'/'.$data['name']);
         } catch (\Exception $e) {
             // Fail!
-            $errors = $file->getErrors();
+            $errors = $e->getMessage();
             return $this->ajaxReturn(1, '图片上传失败');
         }
     }
